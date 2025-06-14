c9450041adbf4a6f12e7cb12c70096e05bf84371
Sugat Ghimire <sugat1237@gmail.com>
2025-06-14 13:29:13 +0545

    first commit here

---
+++ src/main/java/com/sugat/spring_restapi_demo/SpringRestapiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sugat.spring_restapi_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestapiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestapiDemoApplication.class, args);
+	}
+
+}
