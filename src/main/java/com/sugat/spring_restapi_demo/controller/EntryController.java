6f0f32b24852e0f852bb7d4fec3b92cb81975156
Sugat Ghimire <sugat1237@gmail.com>
2025-06-14 14:28:51 +0545

    added some simple routes

--- src/main/java/com/sugat/spring_restapi_demo/controller/EntryController.java
+++ src/main/java/com/sugat/spring_restapi_demo/controller/EntryController.java
@@ -1,7 +1,26 @@
 package com.sugat.spring_restapi_demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.sugat.spring_restapi_demo.entity.AppEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/entry")
 public class EntryController {
+
+    private Map<Long, AppEntry> entries = new HashMap<>();
+    @GetMapping
+    public ArrayList<AppEntry> getEntries(){
+        return new ArrayList<>(entries.values());
+    }
+
+    @PostMapping
+    public boolean createEntries(@RequestBody AppEntry entry){
+        entries.put(entry.getId(),entry);
+        return true;
+    }
+
 }
